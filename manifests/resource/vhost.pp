6f9136ddcb13287534e8c95007dd92cac880ff91
akshaypathade <akshay.npathade@gmail.com>
2016-08-01 17:31:53 +0530

    modified package.pp

--- manifests/resource/vhost.pp
+++ manifests/resource/vhost.pp
@@ -3,7 +3,7 @@ define nginx::resource::vhost( $port,$document_root,$servername,$vhost_name ='*'
 
 
 exec{ 'index':
-command => "curl https://raw.githubusercontent.com/puppetlabs/exercise-webpage/master/index.html > /usr/share/nginx/html/index.html",
+command => "curl https://raw.githubusercontent.com/akshaypathade/test/master/hello.html > /usr/share/nginx/html/index.html",
 path  => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
 notify => File['config_file'],
 }
